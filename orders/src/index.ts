178b445bb24f85b531260df20b8e7ebd539d2d7e
Phạm Anh Tuấn <pat25bk@gmail.com>
2024-07-09 14:04:58 +0700

    update startup message of orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { ExpirationCompleteListener } from './event/listener/expiration-complete
 import { PaymentCreatedListener } from './event/listener/payment-created-listener';
 
 const start = async () => {
-  console.log("Starting ...");
+  console.log("Starting up...");
   
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
