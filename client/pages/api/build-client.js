eb77f0d1f484970c68163d17858153fa95d771f3
Phạm Anh Tuấn <pat25bk@gmail.com>
2024-07-12 10:34:40 +0700

    update URL in client and add more workflow for each service

--- client/pages/api/build-client.js
+++ client/pages/api/build-client.js
@@ -4,7 +4,8 @@ export default ({req})=>{
     if(typeof window === 'undefined'){
         // On the server
         return axios.create({
-            baseURL:'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            // baseURL:'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL:'http://tuananhpham.site/',
             headers:req.headers
         });
     }else{
