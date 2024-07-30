48b6a730fa10567e466e28610383c6ee7d73ba6e
Phạm Anh Tuấn <pat25bk@gmail.com>
2024-07-30 15:33:39 +0700

    fix bug in client

--- client/pages/api/build-client.js
+++ client/pages/api/build-client.js
@@ -4,8 +4,8 @@ export default ({req})=>{
     if(typeof window === 'undefined'){
         // On the server
         return axios.create({
-            baseURL:'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-            // baseURL:'http://tuananhpham.site/',
+            // baseURL:'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL:'http://tuananhpham.site/',
             headers:req.headers
         });
     }else{
